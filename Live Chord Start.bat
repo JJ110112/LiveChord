49c69ea0181aa7dbd1fd4bf8282c653999e808b5
JJ110112 <hipokerwu@gmail.com>
2026-03-25 15:45:29 +0800

    modify batch file to correct folder

--- Live Chord Start.bat
+++ Live Chord Start.bat
@@ -2,6 +2,6 @@
 echo ==========================================
 echo   LiveChord - Starting...
 echo ==========================================
-cd /d "%~dp0backend"
+cd /d "C:\Users\hitea\LiveChord\backend"
 python -m uvicorn main:app --host 0.0.0.0 --port 8800 --reload
 pause
